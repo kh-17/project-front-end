8a9375a0e0fc82257cf9725c10b2a2016dc8c515
unknown <khushi.a2002@gmail.com>
2022-12-29 19:44:55 +0530

    created project app, added homepage folder

---
+++ pastry-website/HomePage/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 150px;
+    color: #00095B;
+    font-family: Lora;
+    
+`;
+
+export const SubWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 300px;
+`
+
+export const Header = styled.div`
+font-size: 20px;
+font-weight: 600;
+`
+
+export const Content = styled.div`
+font-size: 14px;
+font-weight: 400;
+`
+
+export const CakeBox = styled.div`
+display: flex;
+justify-content: center;
+gap: 50px;
+flex-direction: row;
+`
